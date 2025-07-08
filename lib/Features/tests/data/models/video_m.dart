16e12121d6483987dbaca594b41b4a707ea97a1e
Laeth234al <laeth2004alhalabe@gmail.com>
2025-07-08 13:49:56 +0300

    fixed-main

--- lib/Features/tests/data/models/video_m.dart
+++ lib/Features/tests/data/models/video_m.dart
@@ -4,30 +4,45 @@ class VideoModel extends Video {
   VideoModel({
     required super.id,
     required super.url,
+    required super.teacherId,
   });
 
   factory VideoModel.fromJson(Map json) {
     return VideoModel(
       id: json["id"] ?? 0,
       url: json["url"],
+      teacherId : json["teacher_id"] ?? "-",
     );
   }
   factory VideoModel.fromClass(Video video) {
     return VideoModel(
       id: video.id,
       url: video.url,
+      teacherId: video.teacherId,
     );
   }
+  Video toClass() {
+    return Video(
+      id: id,
+      url: url,
+      teacherId: teacherId,
+    );
+  }
+
   factory VideoModel.fromUrl(String url) {
     return VideoModel(
       id: -1,
       url: url,
+      teacherId : "",
     );
   }
-  toJson({bool addId = false,}) {
+  toJson({
+    bool addId = false,
+  }) {
     return {
       if (addId) "id": id,
-      "url" : url,
+      "url": url,
+      "teacher_id": teacherId,
     };
   }
 }
